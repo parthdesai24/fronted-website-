03886b08187dee5aae6260fe5928d66b788386e0
Parth Desai <130673931+parthdesai24@users.noreply.github.com>
2024-09-15 19:02:35 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,37 +1,3 @@
-// function animateValue(element,start,end,duration){
-//     let current = start;
-//     const range = end - start;
-//     const increment = end > start ? 1 : -1;
-//     const stepTime = Math.abs(Math.floor(duration / range));
-//     const timer = setInterval(() => {
-//        current += increment;
-//        element.textContent = current;
-//        if(current ===end){
-//         clearInterval(timer);
-//        }
-//     },stepTime);
-
-// }
-
-// function startCounter(){
-//     const counterElements = document.querySelectorAll('.counter-value');
-//     counterElements.forEach((element)=>{
-//         const target = parseInt (element.getAttribute('data-target'));
-//         animateValue(element, 0, target, 1500)
-//     });
-// }
-// window.addEventListener('scroll',() => {
-//     const counterSection = document.querySelector('.counter');
-//     const counterSectionTop = counterSection.getBoundingClientRect().top;
-//     const windowHeight = window.innerHeight;
-//     if(counterSectionTop < windowHeight){
-//         startCounter();
-//         window.removeEventListener('scroll',startCounter())
-//     }
-// });
-
-//Filter Gallery
-
 const filterbuttons = document.querySelectorAll(".filter-option");
 const gallery = document.querySelectorAll(".gallery .gallery-sides");
 
@@ -52,3 +18,10 @@ filterbuttons.forEach((option) => {
     });
   });
 });
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
